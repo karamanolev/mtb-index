ab78d7c70bfbb74bd6d2114f8fc02f9cba1f3307
Ivailo Karamanolev <ivailo@hyperscience.com>
2015-04-30 18:52:13 +0300

    Work in progress on the mtbbg parser.

--- mtbbg/route_parser.py
+++ mtbbg/route_parser.py
@@ -2,11 +2,17 @@
 from __future__ import unicode_literals, print_function
 from collections import defaultdict
 from decimal import Decimal
+import json
 import os
 import re
 
 from bs4 import BeautifulSoup
 import requests
+
+
+def read_json(path):
+    with open(path, 'r') as f:
+        return json.loads(f.read())
 
 
 def plaintext_parser(v):
@@ -124,28 +130,72 @@ def parse_meta_parts(meta_parts):
     return parsed_meta
 
 
-def parse_page(url, content):
-    if len(content) == 0:
-        raise Exception('Empty HTML file!')
-    if url != 'http://mtb-bg.com/index.php/trails/gpstracks/2570-gpstrack0047-volno-baskaltsi':
-        pass
-        # return
-    # print('Parsing', url)
-    soup = BeautifulSoup(content)
+def find_metas(soup):
+    metas = []
     for p in soup.find_all('p'):
         strings = list(re.sub('[ ]+', ' ', t) for t in p.stripped_strings)
         if 'Изходна точка:' in strings or 'Дължина:' in strings or 'Продължителност:' in strings:
             meta_parts = split_meta_strings(strings)
             parsed_meta = parse_meta_parts(meta_parts)
-            # print(parsed_meta.get('length') or parsed_meta)
+            metas.append(parsed_meta)
+    return metas
+
+
+def find_trace_links(soup):
+    links = []
+    for a in soup.find_all('a'):
+        href = a.get('href')
+        if not href:
+            continue
+        if not href.endswith('.zip') and not href.endswith('.gpx'):
+            continue
+        if 'нарязани до 500' in a.text.lower():
+            continue
+        links.append(href)
+    return links
+
+
+def parse_page(url, content, ignore_errors):
+    if len(content) == 0:
+        raise Exception('Empty HTML file!')
+    # print('Parsing', url)
+    soup = BeautifulSoup(content)
+    parsed_metas = find_metas(soup)
+    # print(parsed_metas)
+    trace_links = find_trace_links(soup)
+    # print(trace_links)
+    if len(trace_links) != 1:
+        print('Error parsing {0}: {1} links found, must be exactly 1.'.format(url, len(trace_links)))
+        return None
+    if not ignore_errors and len(parsed_metas) != 1:
+        print('Error parsing {0}; {1} metadata blocks found, must be exactly 1'.format(
+            url, len(parsed_metas)))
+        return None
+    parsed_meta = parsed_metas[0] if len(parsed_metas) else None
+    return parsed_meta, trace_links[0]
+
+
+def read_routes():
+    input_routes = read_json(os.path.join(exec_root, '..', 'preprocessor', 'input.json'))
+    return {r['link']: r for r in input_routes['routes']}
 
 
 def main():
+    pages_ignore = {}
+    routes = read_routes()
+    with open(os.path.join(exec_root, 'pages_exceptions.txt')) as f:
+        for line in f:
+            parts = line.decode('utf-8').strip().split(': ', 2)
+            pages_ignore[parts[2]] = parts[1]
     with open(os.path.join(exec_root, 'pages.txt')) as f:
         for rel_url in f:
+            rel_url = rel_url.decode('utf-8').strip()
+            exception = pages_ignore.get(rel_url)
+            if exception == 'ignore':
+                continue
             url = u'http://mtb-bg.com' + rel_url.strip()
             content = download_page(url)
-            parse_page(url, content)
+            parse_page(url, content, exception == 'include')
 
 
 if __name__ == '__main__':
